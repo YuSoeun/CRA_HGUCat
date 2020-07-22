9fc69199f30c81ef06edd9ef5c14c1c3c3aa205f
YuSoeun <68419318+YuSoeun@users.noreply.github.com>
2020-07-22 14:49:22 +0900

    로그인 기능 추가

---
+++ app/src/main/java/com/example/CRA_HGUCat/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.CRA_HGUCat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void Goresister(View v)
+    {
+        Intent cmr = new Intent(this, ResisterActivity.class);
+        startActivity(cmr);
+    }
+
+    public void Gohome(View v)
+    {
+        Intent cmr = new Intent(this, home.class);
+        startActivity(cmr);
+    }
+}
