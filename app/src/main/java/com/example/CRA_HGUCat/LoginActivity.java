8b56b97cf971ea9ed69b9e97ca637db439a56e33
최윤성 <choeyunseong@cys-MacBookPro.T-mobile.com>
2020-07-27 19:35:49 +0900

    로그인 기능 완성 =====<오류 해결>======= 자바에서는 str끼리는 ==으로 구분하면 인식하지 못함. 대신 equals 매소드를 통해 그 값이 같음을 인식해야 한다.

--- app/src/main/java/com/example/CRA_HGUCat/LoginActivity.java
+++ app/src/main/java/com/example/CRA_HGUCat/LoginActivity.java
@@ -14,8 +14,12 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
+
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -42,33 +46,58 @@ public class LoginActivity extends AppCompatActivity {
         Id = findViewById(R.id.et_id);
         Password = findViewById(R.id.et_pass);
         final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-        firebaseAuth.signInWithEmailAndPassword(Id.getText().toString() + "@handong.edu", Password.getText().toString())
+        final String EmailID = Id.getText().toString() + "@handong.edu";
+        firebaseAuth.signInWithEmailAndPassword(EmailID, Password.getText().toString())
                     .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(task.isSuccessful())
-                            {
-                                if(firebaseAuth.getCurrentUser().isEmailVerified())
-                                {
-                                    Toast.makeText(LoginActivity.this, "안녕하세요! " +
-                                            FirebaseFirestore.getInstance().collection("UserProfile").document("Nickname")
-                                            .get().getResult().getData()
-                                            + "님!", Toast.LENGTH_LONG).show();
-                                    finish();
-                                }
+                            if(task.isSuccessful()) {
+                                DocumentReference docRef = FirebaseFirestore.getInstance().collection("UserProfile").document("Nickname");
+                                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(LoginActivity.this, "접속 중...", Toast.LENGTH_SHORT).show();
+                                            Map<String, Object> mail2name = task.getResult().getData();
+                                            for (Map.Entry<String, Object> i : mail2name.entrySet()) {
+                                                if (i.getKey().equals(EmailID)) {
+                                                    Toast.makeText(LoginActivity.this, "안녕하세요! " + i.getValue() + "님!", Toast.LENGTH_LONG).show();
+                                                    finish();
+                                                }
+                                            }
+                                        }
+                                    }
+                                });
                             }
-                            else{
-                                if(firebaseAuth.getCurrentUser().isEmailVerified())
-                                {
-                                    Toast.makeText(LoginActivity.this, "비밀번호를 잘못 입력하였습니다.\n비밀번호를 다시 입력해주세요.", Toast.LENGTH_LONG).show();
-                                    Password.setText("");
-                                }
-                                else
-                                {
-                                    Toast.makeText(LoginActivity.this, "등록되지 않은 학번입니다.\n먼저 회원가입을 해주세요.", Toast.LENGTH_LONG).show();
-                                    Id.setText("");
-                                    Password.setText("");
-                                }
+                            else
+                            {
+                                DocumentReference docRef = FirebaseFirestore.getInstance().collection("UserProfile").document("Nickname");
+                                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                        if(task.isSuccessful())
+                                        {
+                                            Map<String, Object> mail2name = task.getResult().getData();
+                                            if(mail2name.containsKey(EmailID))
+                                            {
+                                                Toast.makeText(LoginActivity.this, "비밀번호를 잘못 입력하였습니다.\n비밀번호를 다시 입력해주세요.", Toast.LENGTH_LONG).show();
+                                                Password.setText("");
+                                            }
+                                            else
+                                            {
+                                                Toast.makeText(LoginActivity.this, "등록되지 않은 학번입니다.\n먼저 회원가입을 해주세요.", Toast.LENGTH_LONG).show();
+                                                Id.setText("");
+                                                Password.setText("");
+                                            }
+                                        }
+                                        else
+                                        {
+                                            Toast.makeText(LoginActivity.this, "오류가 발생했습니다.\n다시 시도해주세요.", Toast.LENGTH_LONG).show();
+                                            Id.setText("");
+                                            Password.setText("");
+                                        }
+                                    }
+                                });
                             }
                         }
                     });
